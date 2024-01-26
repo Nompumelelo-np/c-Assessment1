3f8a70bec553c634de278b28c62d3f6db36496eb
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-26 16:11:16 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace c__Assessment1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.BackColor = System.Drawing.Color.Lavender;
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.addButton.Location = new System.Drawing.Point(136, 64);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 69);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = false;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // changeButton
+            // 
+            this.changeButton.BackColor = System.Drawing.Color.Lavender;
+            this.changeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeButton.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.changeButton.Location = new System.Drawing.Point(136, 162);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(75, 68);
+            this.changeButton.TabIndex = 1;
+            this.changeButton.Text = "Change";
+            this.changeButton.UseVisualStyleBackColor = false;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Lavender;
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(136, 257);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 67);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            // 
+            // removeButton
+            // 
+            this.removeButton.BackColor = System.Drawing.Color.Lavender;
+            this.removeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeButton.Location = new System.Drawing.Point(136, 351);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(97, 54);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = false;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Lavender;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(345, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 49);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Lavender;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(319, 136);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(253, 210);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.BackgroundImage = global::c__Assessment1.Properties.Resources.images1;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.addButton);
+            this.Name = "Form1";
+            this.Text = "Words";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.Button addButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace c__Assessment1
+{
+    public partial class Form1 : Form
+    {
+        private List<string> wordsList = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string newWord = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(newWord))
+            {
+                MessageBox.Show("Please enter a word or phrase.");
+                return;
+            }
+
+            if (!wordsList.Contains(newWord))
+            {
+                wordsList.Add(newWord);
+                UpdateListBox();
+                MessageBox.Show("Word added! ");
+            }
+
+            else
+            {
+                MessageBox.Show("Word or phrase already exists. ");
+            }
+
+            ClearTextBox();
+        }
+
+        private void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(wordsList.ToArray());
+        }
+        private string GetSelectedWord()
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                return listBox1.SelectedItem.ToString();
+            }
+            return null;
+        }
+
+        private void ClearTextBox()
+        {
+            textBox1.Clear();
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        {
+            string selectedWord = GetSelectedWord();
+
+            if (string.IsNullOrEmpty(selectedWord))
+            {
+                MessageBox.Show("Please select a word to change.");
+                return;
+            }
+
+            string newWord = textBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(newWord))
+            {
+                MessageBox.Show("Please enter a new word to change!" );
+                return;
+            }
+            int index = wordsList.IndexOf(selectedWord);
+            wordsList[index] = newWord;
+
+            MessageBox.Show("Word is changed! ");
+            UpdateListBox();
+            ClearTextBox();
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            wordsList.Clear();
+            UpdateListBox();
+            MessageBox.Show("All words cleared!.");
+            ClearTextBox();
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            string selectedWord = GetSelectedWord();
+            if (string.IsNullOrEmpty(selectedWord))
+            {
+                MessageBox.Show("Select a word to remove.");
+                return;
+            }
+
+            wordsList.Remove(selectedWord);
+            UpdateListBox();
+            MessageBox.Show("Selected word removed");
+            ClearTextBox();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
